aee2c1f3a872d859c3f1d2e21e2aa0309c63bbec
Matthew Harrilal <matthewharrilal@gmail.com>
2018-10-29 18:17:27 -0700

    Added navbar in main template file

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express()
+var exphbs = require('express-handlebars');
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('./layouts/main', {})
 });
 
+
+// Main.handlebars all other templates inherit from
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+
+// Main Template => main.handlebars
+app.set('view engine', 'handlebars');
 
 app.listen(3000, () => {
     console.log('App listening on port 3000')
