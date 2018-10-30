bc0b15d61e3d9b983020786cd5ffac2996bbf455
Matthew Harrilal <matthewharrilal@gmail.com>
2018-10-30 14:32:55 -0700

    Adding post with createdAt and updatedAt attribute

--- models/post.js
+++ models/post.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
+    createdAt: {
+        type: Date
+    },
+    updatedAt: {
+        type: Date
+    },
     title: {
         type: String,
         required: true
@@ -14,6 +20,17 @@ const PostSchema = new Schema({
         type: String,
         required: true
     }
+});
+
+PostSchema.pre('save', function(next) {
+    const now = new Date()
+    this.updatedAt = now;
+
+    if (!this.createdAt) {
+        this.createdAt = now;
+    }
+
+    next();
 });
 
 module.exports = mongoose.model("Post", PostSchema);
