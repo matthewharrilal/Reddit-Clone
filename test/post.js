f4b6f881017939baec2f9ac596741bfba276206d
Matthew Harrilal <matthewharrilal@gmail.com>
2018-11-05 14:14:34 -0800

    Deleting duplicate test posts

--- test/post.js
+++ test/post.js
@@ -12,22 +12,24 @@ const samplePost = {
 
 describe("Posts", () => {
     it("should create with valid attributes at POST /posts", done => {
-        Post.find(function(err, posts) {
-          var postCount = Object.keys(posts).length;
-          console.log('This is the post count ' + postCount)
-          chai
-            .request(app)
-            .post("/posts")
-            .send(samplePost)
-            .then(res => {
-              Post.find(function(err, posts) {
-                postCount.should.be.equal(posts.length - 1);
-                res.should.have.status(200);
-                return done();
-              });
-            })
-            .catch(err => {
-              return done(err);
+        Post.findOneAndRemove(samplePost, function()  {
+            Post.find(function(err, posts) {
+              var postCount = Object.keys(posts).length;
+              console.log('This is the post count ' + postCount)
+              chai
+                .request(app)
+                .post("/posts")
+                .send(samplePost)
+                .then(res => {
+                  Post.find(function(err, posts) {
+                    postCount.should.be.equal(posts.length - 1);
+                    res.should.have.status(200);
+                    return done();
+                  });
+                })
+                .catch(err => {
+                  return done(err);
+                });
             });
         });
     });
