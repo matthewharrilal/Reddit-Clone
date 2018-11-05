f6681d51ad8b166f19d92e12b92cd0b4e73095bd
Matthew Harrilal <matthewharrilal@gmail.com>
2018-11-05 14:05:57 -0800

    Fixed the test for creating a post object and testing for it

--- server.js
+++ server.js
@@ -48,3 +48,5 @@ app.set('view engine', 'handlebars');
 app.listen(3000, () => {
     console.log('App listening on port 3000')
 });
+
+module.exports = app
--- test/post.js
+++ test/post.js
@@ -1,7 +1,7 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const should = chai.should();
-const app = require("../app.js")
+const app = require("../server.js")
 const Post = require("../models/post")
 
 const samplePost = {
@@ -12,12 +12,42 @@ const samplePost = {
 
 describe("Posts", () => {
     it("should create with valid attributes at POST /posts", done => {
-        const post = new Post(samplePost)
-        chai.request(app)
-            .send(post)
-            .end((err, response, body) => {
-                console.log("ERROR SAVING TEST POST " + err.data)
+        Post.find(function(err, posts) {
+          var postCount = Object.keys(posts).length;
+          console.log('This is the post count ' + postCount)
+          chai
+            .request(app)
+            .post("/posts")
+            .send(samplePost)
+            .then(res => {
+              Post.find(function(err, posts) {
+                postCount.should.be.equal(posts.length - 1);
+                res.should.have.status(200);
+                return done();
+              });
             })
-
+            .catch(err => {
+              return done(err);
+            });
+        });
     });
 });
+
+
+// Post.find(function(err, posts) {
+//   var postCount = posts.count;
+//   chai
+//     .request(app)
+//     .post("/posts")
+//     .send(samplePost)
+//     .then(res => {
+//       Post.find(function(err, posts) {
+//         postCount.should.be.equal(posts.length - 1);
+//         res.should.have.status(200);
+//         return done();
+//       });
+//     })
+//     .catch(err => {
+//       return done(err);
+//     });
+// });
