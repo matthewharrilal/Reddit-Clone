85249515a344b5ad3b74ae9a9e8fb1f60447a6fd
Matthew Harrilal <matthewharrilal@gmail.com>
2018-11-05 15:01:10 -0800

    Able to find all posts on a specific subreddit have to render them in a template

--- controllers/subreddit.js
+++ controllers/subreddit.js
@@ -3,6 +3,9 @@ const Post = require('../models/post')
 
 module.exports = function(app) {
     app.get('/n/:subreddit', function(req, res) {
-        console.log(req.params.subreddit)
+        Post.find({subreddit: req.params.subreddit})
+        .then((posts) => {
+            console.log('These are the posts ' + posts)
+        });
     });
 }
