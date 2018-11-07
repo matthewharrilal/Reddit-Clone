d48a5d8835eb195b805a1190e840f44d162c1dc5
Matthew Harrilal <matthewharrilal@gmail.com>
2018-11-07 14:55:54 -0800

    Added logic to the post request of creating a user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,20 @@
+const User = require('../models/user')
+
 module.exports = (app) => {
     // Sign Up Form
     app.get('/signup', (req, res) => {
         res.render('sign-up');
+    });
+
+    app.post('/signup', (req, res) => {
+        const user = new User(req.body)
+        user
+        .save()
+        .then((user) => {
+            res.redirect('/')
+        })
+        .catch((err) => {
+            console.log('ERROR ' + err)
+        })
     });
 }
