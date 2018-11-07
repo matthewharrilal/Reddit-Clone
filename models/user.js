5d56a89da80e8721048a96e45dde2e6dd3edc77d
Matthew Harrilal <matthewharrilal@gmail.com>
2018-11-07 14:50:33 -0800

    Added user.js and added the attributes

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    createdAt: {
+        type: Date
+    },
+    updatedAt: {
+        type: Date
+    },
+    password: {
+        type: String,
+        select: false
+    },
+    username: {
+        type: String,
+        required: true
+    }
+});
+
+// Before saving the user give them created at and updated at attributes
+UserSchema.pre("save", function (next) {
+    // SET createdAt AND updatedAt
+    const now = new Date();
+    this.updatedAt = now;
+    if (!this.createdAt) {
+        this.createdAt = now;
+    }
+    next();
+});
+
+module.exports = mongoose.model("User", UserSchema);
