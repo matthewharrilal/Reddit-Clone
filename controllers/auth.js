4f56e1cf33cc9313c3914777c91cf8ab3ed75ece
Matthew Harrilal <matthewharrilal@gmail.com>
2018-11-07 19:39:55 -0800

    Able to successfully login

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,8 +34,50 @@ module.exports = (app) => {
         res.render('login.handlebars')
     });
 
+    app.post("/login", (req, res) => {
+        const username = req.body.username; // Grab the username and password credentials
+        const password = req.body.password;
+        // Find this user name
+        User.findOne({ // Find user by the username
+                username
+            }, "username password")
+            .then(user => {
+                if (!user) {
+                    // User not found
+                    return res.status(401).send({
+                        message: "Wrong Username or Password"
+                    });
+                }
+                // Check the password
+                user.comparePassword(password, (err, isMatch) => {
+                    if (!isMatch) {
+                        // Password does not match
+                        return res.status(401).send({
+                            message: "Wrong Username or password"
+                        });
+                    }
+                    // Create a token
+                    const token = jwt.sign({
+                        _id: user._id,
+                        username: user.username
+                    }, process.env.SECRET, {
+                        expiresIn: "60 days"
+                    });
+                    // Set a cookie and redirect to root
+                    res.cookie("nToken", token, {
+                        maxAge: 900000,
+                        httpOnly: true
+                    });
+                    res.redirect("/");
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
+
     app.get('/logout', (req, res) => {
         res.clearCookie('nToken');
         res.redirect('/');
-      });
+    });
 }
