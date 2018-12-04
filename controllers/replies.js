8f14ecdcc6470f5f0d0547c03985cd80f0c683df
Matthew Harrilal <matthewharrilal@gmail.com>
2018-12-03 19:09:35 -0800

    Redirecting form submission to the corresponding replies route

--- controllers/replies.js
+++ controllers/replies.js
@@ -5,23 +5,25 @@ var User = require("../models/user");
 module.exports = app => {
     // NEW REPLY
     app.get("/posts/:postId/comments/:commentId/replies/new", (req, res) => {
-      let post;
-      Post.findById(req.params.postId)
-        .then(p => {
-          post = p;
-          return Comment.findById(req.params.commentId);
-        })
-        .then(comment => {
-            res.json(comment)
-        //   res.render("replies-new", { post, comment });
-        })
-        .catch(err => {
-          console.log(err.message);
-        });
+        let post;
+        Post.findById(req.params.postId)
+            .then(p => {
+                post = p;
+                return Comment.findById(req.params.commentId);
+            })
+            .then(comment => {
+                res.render("replies-new", {
+                    post,
+                    comment
+                });
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
     });
-  
+
     // CREATE REPLY
     app.post("/posts/:postId/comments/:commentId/replies", (req, res) => {
-      console.log(req.body);
+        res.send(req.body);
     });
-  };
+};
